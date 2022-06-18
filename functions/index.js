60714133375694928c35e105a5905debe97936cf
Napuu <santeri.kaariainen@iki.fi>
2022-06-18 17:41:16 +0300

    debug

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 export const onRequestGet = async ({request}) => {
   return Response(JSON.stringify({
     headers: Object.fromEntries(request.headers),
-    cf: request.cf
+    // cf: request.cf
   }), {
     headers: {
       'content-type': 'text/html;charset=UTF-8',

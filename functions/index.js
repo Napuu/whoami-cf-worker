a84d491b642b2e5b370a10ef3d9455d418d0299b
Napuu <santeri.kaariainen@iki.fi>
2022-06-18 17:42:52 +0300

    debug

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 export const onRequestGet = async ({request}) => {
-  return Response(JSON.stringify({
-    headers: Object.fromEntries(request.headers),
+  return Response(JSON.stringify([...request.headers]), {
+  //  headers: Object.fromEntries(request.headers),
     // cf: request.cf
-  }), {
+  //}), {
     headers: {
       'content-type': 'text/html;charset=UTF-8',
     },

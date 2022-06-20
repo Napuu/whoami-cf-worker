85d822151a39f9c4cc80706468741e9b05a145e4
Napuu <santeri.kaariainen@iki.fi>
2022-06-20 09:58:32 +0300

    remove contenttype

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,3 @@
 export function onRequestGet(context) {
-  return new Response(context.request.headers.get('x-real-ip'), { headers: {'content-type': 'text/html; charset=UTF-8'} });
+  return new Response(context.request.headers.get('x-real-ip'));
 }
